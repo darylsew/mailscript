161d4dabadac5de32b3c61cdce45b06e022ad3bc
Hunter Goldstein <hdg28@cornell.edu>
2013-11-17 23:14:55 -0500

    Code is more readable and usr friendly

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Script to poll my emails for print jobs from roommates/friends. Feel free to do 
 ##TO DO
 [x] Get it working  
 [ ] Attempt to reinstantiate connection if imap aborts  
-[ ] Delete email after printing attachment  
-[ ] Only allow emails from certain people to result in print jobs  
-[ ] Encrypt email password (input from config file currently)  
+[x] Delete email after printing attachment  
+[x] Only allow emails from certain people to result in print jobs  
+[x] Encrypt email password (input from config file currently)  
 [ ] Threaded approach? Could be more efficient.  
 [ ] Refactor neatly  
 [ ] Keep track of print volume from different users; calculate costs and charge accordingly  
--- main.py
+++ main.py
@@ -2,49 +2,49 @@ import email, imaplib, os, time
 from printer import default_printer
 
 detach_dir = '.' # directory where to save attachments (default: current)
-user = 'darylprint@gmail.com'
-pwd = open('config.cred').readline().strip()
+user = 'darylprint@gmail.com' #Email to send print reqs to
+pwd = open('config.cred').readline().strip() #Your password location
+exts = ['pdf', 'docx', 'txt','doc', 'odt', 'dvi'] #List of approved exentions
 
 # connecting to the gmail imap server
-m = imaplib.IMAP4_SSL("imap.gmail.com")
+m = imaplib.IMAP4_SSL("imap.gmail.com") #Edit this line w/ your imap server of chocie
 m.login(user,pwd)
-wl = [i.strip for i in open("whitelist.cred").readliens()]
+wl = [i.strip for i in open("whitelist.cred").readlines()]
 
 while True:
-    time.sleep(5)
-    m.select("INBOX") # here you a can choose a mail box like INBOX instead
-    # use m.list() to get all the mailboxes
-    toprint = [i for i in os.listdir('.') if (i.endswith('.pdf') or i.endswith('.docx') or i.endswith('.txt') or i.endswith('.doc'))]
+    time.sleep(1)
+    m.select("INBOX") 	# here you a can choose a mail box like INBOX instead
+    			# use m.list() to get all the mailboxes
+    toprint = [i for i in os.listdir('.') if i.split('.')[-1] in exts]
     if len(toprint) != 0:
         default_printer(toprint[0])
         time.sleep(30)
         os.remove(toprint[0])
     
-    resp, items = m.search(None, 'UNSEEN') # you could filter using the IMAP rules here (check http://www.example-code.com/csharp/imap-search-critera.asp)
-    items = items[0].split() # getting the mails id
+    resp, items = m.search(None, 'UNSEEN') 
+    items = items[0].split() # Getting the mails ID
     
     for emailid in items:
-        resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
-        email_body = data[0][1] # getting the mail content
-        mail = email.message_from_string(email_body) # parsing the mail content to get a mail object
+        resp, data = m.fetch(emailid, "(RFC822)") #(RFC822) gives full contents of mail
+        email_body = data[0][1] # Getting the mail content
+        mail = email.message_from_string(email_body) # Parse email to String
     
 	#Checks to see if the recieved email is in the whitelist
     	if not mail["From"] in wl:
-			print "Recieved email from non-whitelisted user: "+mail["From"]
-			m.store(mail, '+FLAGS', '\\Deleted')
-			m.expunge
-			continue
+	    print "Recieved email from non-whitelisted user: "+mail["From"]
+	    m.store(mail, '+FLAGS', '\\Deleted')
+	    m.expunge
+	    continue
 
         #Check if any attachments at all
         if mail.get_content_maintype() != 'multipart':
-    		continue
+    	    continue
 
         #print 'yo i got attachments'
         if mail["Subject"].lower() == 'print':
     
             #print "["+mail["From"]+"] :" + mail["Subject"]
     
-            # we use walk to create a generator so we can iterate on the parts and forget about the recursive headach
             for part in mail.walk():
                 # multipart are just containers, so we skip them
                 if part.get_content_maintype() == 'multipart':
