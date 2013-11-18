99097e9ad4c4fdec7cb19a56e89908c8c0cf85f3
Daryl Sew <darylsew@gmail.com>
2013-11-17 23:56:35 -0500

    some fixes to whitelisting/etc

--- main.py
+++ main.py
@@ -9,68 +9,72 @@ exts = ['pdf', 'docx', 'txt','doc', 'odt', 'dvi'] #List of approved exentions
 # connecting to the gmail imap server
 m = imaplib.IMAP4_SSL("imap.gmail.com") #Edit this line w/ your imap server of chocie
 m.login(user,pwd)
-wl = [i.strip for i in open("whitelist.cred").readlines()]
+wl = [i.strip() for i in open("whitelist.cred").readlines()]
 
 while True:
     time.sleep(1)
     m.select("INBOX") 	# here you a can choose a mail box like INBOX instead
-    			# use m.list() to get all the mailboxes
+                        # use m.list() to get all the mailboxes
     toprint = [i for i in os.listdir('.') if i.split('.')[-1] in exts]
     if len(toprint) != 0:
         default_printer(toprint[0])
         time.sleep(30)
         os.remove(toprint[0])
-    
+
     resp, items = m.search(None, 'UNSEEN') 
     items = items[0].split() # Getting the mails ID
-    
+
     for emailid in items:
-        resp, data = m.fetch(emailid, "(RFC822)") #(RFC822) gives full contents of mail
-        email_body = data[0][1] # Getting the mail content
-        mail = email.message_from_string(email_body) # Parse email to String
-    
-	#Checks to see if the recieved email is in the whitelist
-    	if not mail["From"] in wl:
-	    print "Recieved email from non-whitelisted user: "+mail["From"]
-	    m.store(mail, '+FLAGS', '\\Deleted')
-	    m.expunge
-	    continue
+        #(RFC822) gives full contents of mail
+        resp, data = m.fetch(emailid, "(RFC822)")
+        if data is not None:
+            # Getting the mail content
+            email_body = data[0][1] 
+            # Parse email to String
+            mail = email.message_from_string(email_body) 
+        else:
+            continue
+
+        #Checks to see if the recieved email is in the whitelist
+        sender = mail["From"].split('<')[1][:-1]
+        if not sender in wl:
+            print "Received email from non-whitelisted user: " + sender
+            m.store(emailid, '+FLAGS', '\\Deleted')
+            m.expunge()
+            continue
 
         #Check if any attachments at all
         if mail.get_content_maintype() != 'multipart':
-    	    continue
+            m.store(emailid, '+FLAGS', '\\Deleted')
+            m.expunge()
+            continue
 
-        #print 'yo i got attachments'
-        if mail["Subject"].lower() == 'print':
-    
-            #print "["+mail["From"]+"] :" + mail["Subject"]
-    
-            for part in mail.walk():
-                # multipart are just containers, so we skip them
-                if part.get_content_maintype() == 'multipart':
-                    continue
-    
-                # is this part an attachment ?
-                if part.get('Content-Disposition') is None:
-                    continue
-    
-                filename = part.get_filename()
-                counter = 1
-    
-                # if there is no filename, we create one with a counter to avoid duplicates
-                if not filename:
-                    filename = 'part-%03d%s' % (counter, 'bin')
-                    counter += 1
-    
-                att_path = os.path.join(detach_dir, filename)
-    
-                #Check if its already there
-                if not os.path.isfile(att_path) :
-                    # finally write the stuff
-                    fp = open(att_path, 'wb')
-                    fp.write(part.get_payload(decode=True))
-                    fp.close()
+        for part in mail.walk():
+            # multipart are just containers, so we skip them
+            if part.get_content_maintype() == 'multipart':
+                continue
 
-		m.store(mail, '+FLAGS', '\\Deleted')
-		m.expunge()
-	    
+            # is this part an attachment ?
+            if part.get('Content-Disposition') is None:
+                continue
+
+            filename = part.get_filename()
+            counter = 1
+
+            # if there is no filename, we create one with a counter to avoid duplicates
+            if not filename:
+                filename = 'part-%03d%s' % (counter, 'bin')
+                counter += 1
+
+            att_path = os.path.join(detach_dir, filename)
+
+            #Check if its already there
+            if not os.path.isfile(att_path) :
+                # finally write the stuff
+                fp = open(att_path, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+
+        m.store(emailid, '+FLAGS', '\\Deleted')
+        m.expunge()
+
